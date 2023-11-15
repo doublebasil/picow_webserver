45e2623c905a35558655428624e25b289af29eb4
Tom Flaherty <tom.flaherty03@gmail.com>
2023-11-15 18:19:03 +0000

    Removed unnecessary includes from main

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,4 @@
-#include <string.h>
-#include <stdlib.h>
-
-#include "lwip/apps/httpd.h"
 #include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"
-
-#include "lwipopts.h" // Required for compile, copied from https://github.com/krzmaz/pico-w-webserver-example, you can find similar files in pico-examples
 
 #include "sysdata.hpp"
 #include "webserver/webserver.hpp"
